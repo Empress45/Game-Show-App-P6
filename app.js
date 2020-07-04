4d23831ae0f555f887f5e9fd57f6b00f69067734
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2020-07-04 09:56:51 -0400

    small changes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // Variables 
 let qwerty = document.getElementById('qwerty');
-let phrase = document.getElementById('phrase');
-let startButton = document.getElementById('.btn_rest');
+let phraseUL = document.getElementById('phrase ul');
+let overlay = document.getElementById('overlay');
+let tries = document.getElementById('scoreboard ol');
+let title = document.getElementById('.title');
+let startGame = document.getElementById('.btn_rest');
 let missed = 0;
