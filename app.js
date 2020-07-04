aed7f2576c1ca7a9aba707f6f56f3af7f0708ea4
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2020-07-04 09:19:53 -0400

    intial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// Variables 
+let qwerty = document.getElementById('qwerty');
+let phrase = document.getElementById('phrase');
+let startButton = document.getElementById('.btn_rest');
+let missed = 0;
