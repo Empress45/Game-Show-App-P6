25fac1a9995157a39a0d3ac8823a014b47321cdf
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2020-07-04 10:15:10 -0400

    adding phrases

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ let tries = document.getElementById('scoreboard ol');
 let title = document.getElementById('.title');
 let startGame = document.getElementById('.btn_rest');
 let missed = 0;
+
+// Phrases Array
+let phrase = ['May the fourth be with you',
+    'A dime a dozen,'
+    'Beating around the bush',
+    'Back to square one,'
+    'A piece of cake',
+]
+
+
+
+
+
+// Listening for start button to be pressed 
+startGame.addEventListener('click', () {
+    overlay.style.display = 'none';
+    resetGame();
+});
