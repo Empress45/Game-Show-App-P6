5f209d15a7adf5d62af94e2133cbec2e9163ac46
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2020-07-06 16:30:07 -0400

    changes

--- app.js
+++ app.js
@@ -12,8 +12,25 @@ const liveHeart = Array.from(document.querySelectorAll('ol li'));
 let missedGuess = 0;
 
 
+// Phrases Array
+const phrases = ["May the force be with you",
+    "You gone learn today",
+    "What you see is what you get",
+    "Happy Anniversary",
+    "Alright Alright Alright"
+];
+
+// Random Phrase Function with Random Number Generator
+function getRandomPhraseAsArray(arr) {
+    let randomNum = phrases.length;
+    getRandomPhraseAsArray(phrases);
+}
+
+
+
 // Listening for click on Start Game Button while hiding overlay
 startGameBtn.addEventListener('click', () => {
     overlay.setAttribute('style', 'display:none');
-    addPhraseToDisplay(phraseArray(phrases));
+    resetGame();
+
 });
