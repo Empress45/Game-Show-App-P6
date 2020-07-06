ce070a6ccceb4f68c5085320b0be9dbf800d0901
Patricia Jackson <47466145+Empress45@users.noreply.github.com>
2020-07-06 16:01:02 -0400

    changes

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
 // Variables 
-let qwerty = document.getElementById('qwerty');
-let phraseUL = document.getElementById('phrase ul');
-let overlay = document.getElementById('overlay');
-let tries = document.getElementById('scoreboard ol');
-let title = document.getElementById('.title');
-let startGame = document.getElementById('.btn_rest');
-let missed = 0;
-
-// Phrases Array
-let phrase = ["May the fourth be with you",
-    "Time waits for no man",
-    "They want to know us, so they can kill us",
-    "A dime a dozen",
-    "Piece of cake",
-    "You gone learn today",
-    "Keep it moving"
-]
-
-// Choosing a random phrase from the phrases array
-function getRandomPhraseAsArray() {
-    // length of array based on random number
-    let randomNum = phrases.length;
-    getRandomPhraseAsArray(phrases);
-}
+const phrase = document.getElementById('#phrase');
+const qwerty = document.getElementById('qwerty');
+const phraseUL = document.querySelector('#phrase ul');
+const startGameBtn = document.querySelector('.btn_reset');
+const overlay = document.getElementById('overlay');
+let keyboardBtn = Array.from(document.querySelectorAll('.keyrow button'));
+const liveHeart = Array.from(document.querySelectorAll('ol li'));
 
 
-function addPhraseToDisplay(phrase) {
-    for (let i = 0; i < phrases.length; i++) {
-        const li = document.createElement(li);
-        li.textContent = phrase[i];
-        if (phrase[i] === "") {
-            li.className = "space";
-        } else {
-            li.className = "letter";
-        }
-        phrase.appendChild(li);
-    }
-}
+// Guesses Missed initialized with 0
+let missedGuess = 0;
 
-// Listening for start button to be pressed 
-startGame.addEventListener('click', () => {
-    startGame.style.display = 'none';
+
+// Listening for click on Start Game Button while hiding overlay
+startGameBtn.addEventListener('click', () => {
+    overlay.setAttribute('style', 'display:none');
+    addPhraseToDisplay(phraseArray(phrases));
 });
